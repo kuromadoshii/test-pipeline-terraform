931dc6eae09be14aaa5f1fb3ed01ea1e3993ca1b
kuromadoshii <viniciusalexander25@gmail.com>
2023-04-11 11:20:39 -0300

    V2

--- vm.tf
+++ vm.tf
@@ -4,7 +4,7 @@ resource "aws_key_pair" "key" {
 }
 resource "aws_instance" "vm" {
   ami                         = "ami-0747e613a2a1ff483"
-  instance_type               = "t2.micro"
+  instance_type               = "t3.nano"
   key_name                    = aws_key_pair.key.key_name
   subnet_id                   = data.terraform_remote_state.vpc.outputs.subnet_id
   vpc_security_group_ids      = [data.terraform_remote_state.vpc.outputs.security_group_id]
